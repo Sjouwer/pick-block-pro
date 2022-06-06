d5767656b728e620ca592e1bbf48f40bfdb74a14
Sjouwer <sjouwer1@gmail.com>
2022-06-07 01:19:57 +0200

    Improved config file Now properly implements categories

--- src/main/java/io/github/sjouwer/pickblockpro/config/ModConfig.java
+++ src/main/java/io/github/sjouwer/pickblockpro/config/ModConfig.java
@@ -1,6 +1,6 @@
 package io.github.sjouwer.pickblockpro.config;
 
-import io.github.sjouwer.pickblockpro.picker.ToolPicker;
+import io.github.sjouwer.pickblockpro.picker.ToolPicker.Tools;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry.Category;
@@ -18,72 +18,54 @@ import java.util.Map;
 @SuppressWarnings("FieldMayBeFinal")
 @Config(name = "pick_block_pro")
 public class ModConfig implements ConfigData {
-    @Tooltip @Category("block_picker_settings")
-    private int blockPickRange = 100;
-    @Tooltip @Category("block_picker_settings")
-    private boolean blockPickBlocks = true;
-    @Tooltip @Category("block_picker_settings")
-    private boolean blockPickEntities = true;
-    @Tooltip @Category("block_picker_settings")
-    private boolean blockPickFluids = false;
-    @Tooltip @Category("block_picker_settings")
-    private boolean blockPickFire = true;
-    @Tooltip @Category("block_picker_settings")
-    private boolean blockPickLight = true;
+    static class BlockPicker {
+        @Tooltip
+        private int range = 100;
+        @Tooltip
+        private boolean pickBlocks = true;
+        @Tooltip
+        private boolean pickEntities = true;
+        @Tooltip
+        private boolean pickFluids = false;
+        @Tooltip
+        private boolean pickFire = true;
+        @Tooltip
+        private boolean pickLight = true;
+    }
 
-    @Tooltip @Category("id_picker_settings")
-    private int idPickRange = 100;
-    @Tooltip @Category("id_picker_settings")
-    private boolean idPickFluids = false;
-    @Tooltip @Category("id_picker_settings")
-    private boolean idPickBlocks = true;
-    @Tooltip @Category("id_picker_settings")
-    private boolean idPickEntities = true;
-    @Tooltip @Category("id_picker_settings")
-    private boolean addNamespace = false;
-    @Tooltip @Category("id_picker_settings")
-    private boolean addProperties = true;
-    @Tooltip @Category("id_picker_settings")
-    private boolean copyToClipboard = true;
+    static class IdPicker {
+        @Tooltip
+        private int range = 100;
+        @Tooltip
+        private boolean pickBlocks = true;
+        @Tooltip
+        private boolean pickEntities = true;
+        @Tooltip
+        private boolean pickFluids = false;
+        @Tooltip
+        private boolean addNamespace = false;
+        @Tooltip
+        private boolean addProperties = true;
+        @Tooltip
+        private boolean copyToClipboard = true;
+    }
 
-    @Tooltip @Category("tool_picker_settings")
-    private int toolPickRange = 100;
-    @Tooltip @Category("tool_picker_settings")
-    private boolean toolPickFluids = false;
-    @Tooltip @Category("tool_picker_settings")
-    private boolean preferSilkTouch = true;
-    @Tooltip @Category("tool_picker_settings")
-    private boolean preferEfficiency = false;
-    @Tooltip @Category("tool_picker_settings")
-    private boolean preferSwordForBamboo = true;
-    @Tooltip @Category("tool_picker_settings")
-    private boolean enchantTools = true;
-
-    static class BaseEnch {
+    static class BaseEnchantments {
         private int unbreaking = 3;
         private int efficiency = 5;
         private boolean mending = true;
     }
-    static class ExtendedEnch {
+
+    static class ExtendedEnchantments {
         @TransitiveObject
-        private BaseEnch base = new BaseEnch();
+        private BaseEnchantments base = new BaseEnchantments();
         @Tooltip
         private boolean silkTouch = true;
         @Tooltip
         private int fortune = 3;
     }
-    @CollapsibleObject @Category("tool_picker_settings")
-    private ExtendedEnch pickEnchantments = new ExtendedEnch();
-    @CollapsibleObject @Category("tool_picker_settings")
-    private ExtendedEnch axeEnchantments = new ExtendedEnch();
-    @CollapsibleObject @Category("tool_picker_settings")
-    private ExtendedEnch shovelEnchantments = new ExtendedEnch();
-    @CollapsibleObject @Category("tool_picker_settings")
-    private ExtendedEnch hoeEnchantments = new ExtendedEnch();
-    @CollapsibleObject @Category("tool_picker_settings")
-    private BaseEnch shearEnchantments = new BaseEnch();
 
-    static class SwordEnch {
+    static class SwordEnchantments {
         private int unbreaking = 3;
         private int sharpness = 5;
         @Tooltip
@@ -96,119 +78,156 @@ public class ModConfig implements ConfigData {
         private int knockback = 0;
         private boolean mending = true;
     }
-    @CollapsibleObject @Category("tool_picker_settings")
-    private SwordEnch swordEnchantments = new SwordEnch();
 
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot1 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot2 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot3 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot4 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot5 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot6 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot7 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot8 = false;
-    @Tooltip @Category("inventory_settings")
-    private boolean lockSlot9 = false;
+    static class ToolPicker {
+        @Tooltip
+        private int range = 100;
+        @Tooltip
+        private boolean pickFluids = false;
+        @Tooltip
+        private boolean preferSilkTouch = true;
+        @Tooltip
+        private boolean preferEfficiency = false;
+        @Tooltip
+        private boolean preferSwordForBamboo = true;
+        @Tooltip
+        private boolean enchantTools = true;
+        @CollapsibleObject
+        private ExtendedEnchantments pickaxeEnchantments = new ExtendedEnchantments();
+        @CollapsibleObject
+        private ExtendedEnchantments axeEnchantments = new ExtendedEnchantments();
+        @CollapsibleObject
+        private ExtendedEnchantments shovelEnchantments = new ExtendedEnchantments();
+        @CollapsibleObject
+        private ExtendedEnchantments hoeEnchantments = new ExtendedEnchantments();
+        @CollapsibleObject
+        private BaseEnchantments shearEnchantments = new BaseEnchantments();
+        @CollapsibleObject
+        private SwordEnchantments swordEnchantments = new SwordEnchantments();
+
+    }
+
+    static class Inventory {
+        @Tooltip
+        private boolean lockSlot1 = false;
+        @Tooltip
+        private boolean lockSlot2 = false;
+        @Tooltip
+        private boolean lockSlot3 = false;
+        @Tooltip
+        private boolean lockSlot4 = false;
+        @Tooltip
+        private boolean lockSlot5 = false;
+        @Tooltip
+        private boolean lockSlot6 = false;
+        @Tooltip
+        private boolean lockSlot7 = false;
+        @Tooltip
+        private boolean lockSlot8 = false;
+        @Tooltip
+        private boolean lockSlot9 = false;
+    }
+
+    @TransitiveObject @Category("block_picker_settings")
+    private BlockPicker blockPicker = new BlockPicker();
+    @TransitiveObject @Category("id_picker_settings")
+    private IdPicker idPicker = new IdPicker();
+    @TransitiveObject @Category("tool_picker_settings")
+    private ToolPicker toolPicker = new ToolPicker();
+    @TransitiveObject @Category("inventory_settings")
+    private Inventory inventory = new Inventory();
 
     public int blockPickRange() {
-        return blockPickRange;
-    }
-
-    public int idPickRange() {
-        return idPickRange;
-    }
-
-    public int toolPickRange() {
-        return toolPickRange;
-    }
-
-    public boolean blockPickFluids() {
-        return blockPickFluids;
-    }
-
-    public boolean idPickFluids() {
-        return idPickFluids;
-    }
-
-    public boolean toolPickFluids() {
-        return toolPickFluids;
-    }
-
-    public boolean blockPickFire() {
-        return blockPickFire;
-    }
-
-    public boolean blockPickLight() {
-        return blockPickLight;
+        return blockPicker.range;
     }
 
     public boolean blockPickBlocks() {
-        return blockPickBlocks;
-    }
-
-    public boolean idPickBlocks() {
-        return idPickBlocks;
+        return blockPicker.pickBlocks;
     }
 
     public boolean blockPickEntities() {
-        return blockPickEntities;
+        return blockPicker.pickEntities;
+    }
+
+    public boolean blockPickFluids() {
+        return blockPicker.pickFluids;
+    }
+
+    public boolean blockPickFire() {
+        return blockPicker.pickFire;
+    }
+
+    public boolean blockPickLight() {
+        return blockPicker.pickLight;
+    }
+
+    public int idPickRange() {
+        return idPicker.range;
+    }
+
+    public boolean idPickBlocks() {
+        return idPicker.pickBlocks;
     }
 
     public boolean idPickEntities() {
-        return idPickEntities;
+        return idPicker.pickEntities;
+    }
+
+    public boolean idPickFluids() {
+        return idPicker.pickFluids;
     }
 
     public boolean addNamespace() {
-        return addNamespace;
+        return idPicker.addNamespace;
     }
 
     public boolean addProperties() {
-        return addProperties;
+        return idPicker.addProperties;
     }
 
     public boolean copyToClipboard() {
-        return copyToClipboard;
+        return idPicker.copyToClipboard;
+    }
+
+    public int toolPickRange() {
+        return toolPicker.range;
+    }
+
+    public boolean toolPickFluids() {
+        return toolPicker.pickFluids;
     }
 
     public boolean preferSilkTouch() {
-        return  preferSilkTouch;
+        return  toolPicker.preferSilkTouch;
     }
 
     public boolean preferEfficiency() {
-        return  preferEfficiency;
+        return  toolPicker.preferEfficiency;
     }
 
     public boolean preferSwordForBamboo() {
-        return preferSwordForBamboo;
+        return toolPicker.preferSwordForBamboo;
     }
 
-    public Map<Enchantment, Integer> getEnchantments(ToolPicker.Tools tool, EntityGroup eGroup) {
-        if (!enchantTools) {
+    public Map<Enchantment, Integer> getEnchantments(Tools tool, EntityGroup eGroup) {
+        if (!toolPicker.enchantTools) {
             return Collections.emptyMap();
         }
 
         HashMap<Enchantment, Integer> enchantments = new HashMap<>();
         switch (tool) {
-            case PICKAXE -> getExtendedEnchantments(pickEnchantments, enchantments);
-            case AXE -> getExtendedEnchantments(axeEnchantments, enchantments);
-            case SHOVEL -> getExtendedEnchantments(shovelEnchantments, enchantments);
-            case HOE -> getExtendedEnchantments(hoeEnchantments, enchantments);
-            case SHEARS -> getBaseEnchantments(shearEnchantments, enchantments);
-            case SWORD -> getSwordEnchantments(swordEnchantments, enchantments, eGroup);
+            case PICKAXE -> getExtendedEnchantments(toolPicker.pickaxeEnchantments, enchantments);
+            case AXE -> getExtendedEnchantments(toolPicker.axeEnchantments, enchantments);
+            case SHOVEL -> getExtendedEnchantments(toolPicker.shovelEnchantments, enchantments);
+            case HOE -> getExtendedEnchantments(toolPicker.hoeEnchantments, enchantments);
+            case SHEARS -> getBaseEnchantments(toolPicker.shearEnchantments, enchantments);
+            case SWORD -> getSwordEnchantments(toolPicker.swordEnchantments, enchantments, eGroup);
         }
 
         return enchantments;
     }
 
-    private void getBaseEnchantments(BaseEnch base, HashMap<Enchantment, Integer> enchantments) {
+    private void getBaseEnchantments(BaseEnchantments base, HashMap<Enchantment, Integer> enchantments) {
         if (base.efficiency > 0) {
             enchantments.put(Enchantments.EFFICIENCY, Math.min(base.efficiency, 255));
         }
@@ -220,9 +239,9 @@ public class ModConfig implements ConfigData {
         }
     }
 
-    private void getExtendedEnchantments(ExtendedEnch extended, HashMap<Enchantment, Integer> enchantments) {
+    private void getExtendedEnchantments(ExtendedEnchantments extended, HashMap<Enchantment, Integer> enchantments) {
         getBaseEnchantments(extended.base, enchantments);
-        if (extended.silkTouch && (extended.fortune <= 0 || preferSilkTouch)) {
+        if (extended.silkTouch && (extended.fortune <= 0 || toolPicker.preferSilkTouch)) {
             enchantments.put(Enchantments.SILK_TOUCH, 1);
         }
         else if (extended.fortune > 0) {
@@ -230,7 +249,7 @@ public class ModConfig implements ConfigData {
         }
     }
 
-    private void getSwordEnchantments(SwordEnch sword, HashMap<Enchantment, Integer> enchantments, EntityGroup eGroup) {
+    private void getSwordEnchantments(SwordEnchantments sword, HashMap<Enchantment, Integer> enchantments, EntityGroup eGroup) {
         if (sword.unbreaking > 0) {
             enchantments.put(Enchantments.UNBREAKING, Math.min(sword.unbreaking, 255));
         }
@@ -262,15 +281,15 @@ public class ModConfig implements ConfigData {
 
     public boolean isSlotLocked(int slot) {
         return switch (slot) {
-            case 0 -> lockSlot1;
-            case 1 -> lockSlot2;
-            case 2 -> lockSlot3;
-            case 3 -> lockSlot4;
-            case 4 -> lockSlot5;
-            case 5 -> lockSlot6;
-            case 6 -> lockSlot7;
-            case 7 -> lockSlot8;
-            case 8 -> lockSlot9;
+            case 0 -> inventory.lockSlot1;
+            case 1 -> inventory.lockSlot2;
+            case 2 -> inventory.lockSlot3;
+            case 3 -> inventory.lockSlot4;
+            case 4 -> inventory.lockSlot5;
+            case 5 -> inventory.lockSlot6;
+            case 6 -> inventory.lockSlot7;
+            case 7 -> inventory.lockSlot8;
+            case 8 -> inventory.lockSlot9;
             default -> false;
         };
     }
